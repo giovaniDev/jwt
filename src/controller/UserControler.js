83a6e72b29ea09a50d1fb6da52cfffc62d005fcc
girvX <giovani.rdgs@gmail.com>
2019-06-19 22:45:47 -0300

    Add a funçao findAllUser para buscar todos

--- src/controller/UserControler.js
+++ src/controller/UserControler.js
@@ -4,5 +4,9 @@ module.exports = {
     async createUser(req, res) {
         const user = await UserSchema.create(req.body);
         return res.json(user);
+    },
+    async findAllUsers(req, res) {
+        const users = await UserSchema.find();
+        return res.json(users);
     }
 }
