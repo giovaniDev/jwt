ca0a5e2390fe1cbca9de045996e889e9ec7536d3
girvX <giovani.rdgs@gmail.com>
2019-06-19 22:54:43 -0300

    Setando email como unique

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
+
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
     }
-})
+});
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 module.exports = mongoose.model('User', UserSchema);
