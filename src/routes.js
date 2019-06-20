fa304e6ec3757b1716e4279515991ce604fa5e4a
girvX <giovani.rdgs@gmail.com>
2019-06-20 15:02:32 -0300

    Adicionado a rota para buscar user por id

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const routes = express.Router();
 
 routes.post('/createuser', UserController.createUser);
 routes.get('/finduser', UserController.findAllUsers);
+routes.get('/finduser/:id', UserController.findUserByID);
 
 module.exports = routes;
