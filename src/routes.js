4e7779fcae37a621a694d500dfafd4da57b5aaa8
girvX <giovani.rdgs@gmail.com>
2019-06-20 16:09:20 -0300

    Nova rota para autentificaçao

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.post('/createuser', UserController.createUser);
 routes.get('/finduser', UserController.findAllUsers);
 routes.get('/finduser/:id', UserController.findUserByID);
+routes.post('/auth', UserController.authUser);
 
 module.exports = routes;
