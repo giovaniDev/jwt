f0d05ef0cecbc798716d1a1c3a69a3ca4ff09e5d
girvX <giovani.rdgs@gmail.com>
2019-06-25 19:22:16 -0300

    Exportando o middleware de autentificação JWT

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const UserController = require('./controller/UserControler');
+const authUser = require('./middlewares/auth');
 
 const routes = express.Router();
 
 routes.post('/createuser', UserController.createUser);
-routes.get('/finduser', UserController.findAllUsers);
-routes.get('/finduser/:id', UserController.findUserByID);
 routes.post('/auth', UserController.authUser);
+
+//rotas que precisam de autentificação
+routes.use(authUser);
+routes.get('/finduser', UserController.findAllUsers);
+routes.get('/painel', (req, res) => res.send('Painel HELLO WORLD!'));
+routes.get('/finduser/:id', UserController.findUserByID);
 
 module.exports = routes;
